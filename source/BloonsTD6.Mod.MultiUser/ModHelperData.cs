1ce40ed12892c385edbbf8d1a28da2689cc6c210
GrahamKracker <gjguzynski@gmail.com>
2024-12-18 17:30:37 -0500

    update description

--- source/BloonsTD6.Mod.MultiUser/ModHelperData.cs
+++ source/BloonsTD6.Mod.MultiUser/ModHelperData.cs
@@ -1,7 +1,7 @@
 ﻿namespace BloonsTD6.Mod.MultiUser;
 public static class ModHelperData {
     internal const string Name = "MultiUser";
-    internal const string Description = "Originally made by Sewer56, this mod allows users to launch multiple instances of.";
+    internal const string Description = "Originally made by Sewer56, this mod allows users to launch multiple instances of the game, with the option to use separate accounts.";
     internal const string Version = "2.0.0";
     internal const string RepoOwner = "Sewer56";
     internal const string RepoName = "BloonsTD6.Mod.MultiUser";
